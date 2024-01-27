6079dae2fba06117483682f680a268a986312d80
Anastasiia Tsymbal <97917919+inaprel3@users.noreply.github.com>
2024-01-27 15:27:18 +0100

    Update Company.cs

--- Hello/Company.cs
+++ Hello/Company.cs
@@ -1,7 +1,7 @@
 public class Company
 {
-    public int CompanyId { get; set; } = 0; // Значение по умолчанию для int - 0
-    public string Name { get; set; } = ""; // Значение по умолчанию для string - пустая строка
-    public string Location { get; set; } = ""; // Значение по умолчанию для string - пустая строка
-    public int YearFounded { get; set; } = 0; // Значение по умолчанию для int - 0
+    public int CompanyId { get; set; } = 0; 
+    public string Name { get; set; } = "";
+    public string Location { get; set; } = "";
+    public int YearFounded { get; set; } = 0; 
 }
